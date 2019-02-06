613f1784dcacab1560e0c2bf95137d677c9f21fe
Dashaway <hdtxun@gmail.com>
2019-02-06 21:48:53 +0800

    主频提升至23Mhz

--- TEST1.c
+++ TEST1.c
@@ -7,18 +7,21 @@
 
 //定时器亮灯
 
+//频率设置
+
 unsigned int I;
 
 void IO_SET();
+void CLK_SET();
 void TIMER_A_SET();
 
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;
 	
-	//端口设置
+	//初始设置
 	IO_SET();
-	
+	CLK_SET();
 	TIMER_A_SET();
 	
 	P1OUT |= BIT0;
@@ -48,6 +51,30 @@ void IO_SET()
 
 }
 
+void CLK_SET()
+{
+	//SMCLK频率设置：32768*2*352 ~23Mhz
+	//MCLK频率设置：32768*2*352 ~23Mhz
+	
+	//清空DCOx和MODx
+	UCSCTL0 = 0;
+	//SMCLK和MCLK选择DCOCLK
+	UCSCTL4_L = SELS_3 +SELM_3;
+	//禁止FLL
+	__bis_SR_register(SCG0);
+	//DCO频率范围6~54Mhz
+	UCSCTL1 = DCORSEL_5;
+	//FLL预先2分频
+	//UCSCTL2 = 0x2000;
+	//倍频系数
+	UCSCTL2 |= 352-1;
+	//使能FLL
+	__bic_SR_register(SCG0);
+	//延时等待稳定
+	__delay_cycles(250000);
+	
+}
+
 void TIMER_A_SET()
 {
 	//SMCLK，增计数，清除TAR
@@ -64,7 +91,7 @@ __interrupt void TIMER_A(void)
 {
 	//TIMER_A中断
 	I++;
-	if(I == 16)
+	if(I == 176)
 	{
 		I = 0;
 		P1OUT ^= BIT0;
