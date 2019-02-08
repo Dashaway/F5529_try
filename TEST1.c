0e8848f266b1a798b9b7fede99db97fa62ced606
Dashaway <hdtxun@gmail.com>
2019-02-08 16:40:10 +0800

    提升内核电压 主频提升至50Mhz

--- TEST1.c
+++ TEST1.c
@@ -1,5 +1,6 @@
 #include "msp430f5529.h"
 
+
 //190206
 //按键亮灯
 
@@ -12,6 +13,7 @@
 unsigned int I;
 
 void IO_SET();
+void SetVcoreUp(unsigned int level);
 void CLK_SET();
 void TIMER_A_SET();
 
@@ -51,10 +53,39 @@ void IO_SET()
 
 }
 
+
+void SetVcoreUp(unsigned int level)
+{
+	//内核电压调整
+	
+	// Open PMM registers for write
+	PMMCTL0_H = PMMPW_H;              
+	// Set SVS/SVM high side new level
+	SVSMHCTL = SVSHE + SVSHRVL0*level + SVMHE + SVSMHRRL0*level;
+	// Set SVM low side to new level
+	SVSMLCTL = SVSLE + SVMLE + SVSMLRRL0*level;
+	// Wait till SVM is settled
+	while ((PMMIFG & SVSMLDLYIFG) == 0);
+	// Clear already set flags
+	PMMIFG &= ~(SVMLVLRIFG + SVMLIFG);
+	// Set VCore to new level
+	PMMCTL0_L = PMMCOREV0*level;
+	// Wait till new level reached
+	if((PMMIFG & SVMLIFG))
+		while((PMMIFG & SVMLVLRIFG) == 0);
+	// Set SVS/SVM low side to new level
+	SVSMLCTL = SVSLE + SVSLRVL0*level + SVMLE + SVSMLRRL0*level;
+	// Lock PMM registers for write access
+	PMMCTL0_H = 0x00;
+}
+
+
 void CLK_SET()
 {
-	//SMCLK频率设置：32768*2*352 ~23Mhz
-	//MCLK频率设置：32768*2*352 ~23Mhz
+	//SMCLK频率设置：32768*2*770 ~50Mhz
+	//MCLK频率设置：32768*2*770 ~50Mhz
+	
+	SetVcoreUp(0x03);
 	
 	//清空DCOx和MODx
 	UCSCTL0 = 0;
@@ -63,15 +94,16 @@ void CLK_SET()
 	//禁止FLL
 	__bis_SR_register(SCG0);
 	//DCO频率范围6~54Mhz
-	UCSCTL1 = DCORSEL_5;
+	UCSCTL1 = DCORSEL_6;
 	//FLL预先2分频
-	//UCSCTL2 = 0x2000;
 	//倍频系数
-	UCSCTL2 |= 352-1;
+	UCSCTL2 = FLLD_1 + 770-1;
 	//使能FLL
 	__bic_SR_register(SCG0);
 	//延时等待稳定
-	__delay_cycles(250000);
+	__delay_cycles(400000);
+	
+	
 	
 }
 
@@ -91,7 +123,7 @@ __interrupt void TIMER_A(void)
 {
 	//TIMER_A中断
 	I++;
-	if(I == 176)
+	if(I == 770)
 	{
 		I = 0;
 		P1OUT ^= BIT0;
